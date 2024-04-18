e87ccf9b4cd4fb8c881b22cb7abb7a1b09188a28
Kelly Glass <73360394+kellyglass10@users.noreply.github.com>
2024-04-18 15:37:35 -0700

    added-footer-links
    added github and linkedin link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        Site created by{" "}
+        <a
+          href="https://www.linkedin.com/in/kelly-glass-9b86a7284/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Kelly Glass.
+        </a>{" "}
+        Available on{" "}
+        <a
+          href="https://github.com/kellyglass10/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github.
+        </a>
+      </footer>
+    </div>
+  );
 }
