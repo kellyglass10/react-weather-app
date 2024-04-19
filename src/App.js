3b6c4b8330eea4936e7e2236c1a73195390b8d4f
Kelly Glass <73360394+kellyglass10@users.noreply.github.com>
2024-04-19 10:57:59 -0700

    added-bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Site created by{" "}
-        <a
-          href="https://www.linkedin.com/in/kelly-glass-9b86a7284/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Kelly Glass.
-        </a>{" "}
-        Available on{" "}
-        <a
-          href="https://github.com/kellyglass10/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Site created by{" "}
+          <a
+            href="https://www.linkedin.com/in/kelly-glass-9b86a7284/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Kelly Glass.
+          </a>{" "}
+          Available on{" "}
+          <a
+            href="https://github.com/kellyglass10/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
