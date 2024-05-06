5b997662944261d5100a0c849344b3d3d668c2fb
Kelly Glass <73360394+kellyglass10@users.noreply.github.com>
2024-05-06 15:34:46 -0700

    create-forecast-component/css-file
    displaying one day of forecast, no api data involved.
    bug: need to fix weathericon, not aligning with other text

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,7 +17,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div className="float-left">
-              <WeatherIcon code={props.data.icon} />
+              <WeatherIcon code={props.data.icon} siz={60} />
             </div>
 
             <div className="float-left">
